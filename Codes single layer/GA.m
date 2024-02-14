cf1d846bdb9b0218528863fc83930507d2081f76
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2024-02-14 17:00:33 +0100

    Add files via upload

--- Codes single layer/GA.m
+++ Codes single layer/GA.m
@@ -1,85 +1,104 @@
-function [vec_opt,fval]=GA(objectif,vec_ini,tol);
+function [vec_opt,fval]=GA(objectif,vec_ini,tol)
 tic
 % parametres
- nb_pop = 20;           % nb dans la population
- nb_crois = 5;          % nb meilleurs autorisÈs ‡ se croiser
- p_mutation = 0.5;      % proba de mutation
- p_croisement = 0.5;    % proba de croisement
- borne = 0.1;           % intervalle de recherche x,y=[-val val]
+nb_pop = 10;           % nombre d'individus dans la population
+nb_crois = 3;          % nombre des meilleurs individus autoris√©s √† se croiser
+p_mutation = 0.5;      % probabilit√© de mutation
+p_croisement = 0.5;    % probabilit√© de croisement
+borne = 0.1;           % intervalle de recherche x,y=[-val val]
+max_steps = 15;        % nombre d'√©tapes maximales pour l'algo gen avant de relancer
 
- 
-%On rÈcupËre la taille du vecteur des donnÈes ‡ optimiser
+options = optimset('display','off',...         % affichage des infos √† chaque it√©ration
+    'largescale','on',...        % A pr√©ciser
+    'MaxFunEvals',2000, ...      % nb max evaluation de fonction
+    'MaxIter',    2000, ...      % nb max  d'it√©ration
+    'TolFun',     1e-12,  ...    % valeur critique fonction
+    'TolX',       1e-12);        % tol√©rence it√©r√©s
+
+%On r√©cup√®re la taille du vecteur des donn√©es √† optimiser
 nb_var=vec_ini;
 val=1;
 % initialisation population et nb de cycle
 vec_objectif=zeros(nb_pop,1);
-%val=zeros(nb_cycle,1);
 pop=zeros(nb_pop, nb_var);
 
-options = optimset('display','off',...         % affichage des infos ‡ chaque itÈration
-                   'largescale','off',...       % A prÈciser
-                   'MaxFunEvals',2000, ...      % nb max evaluation de fonction
-                   'MaxIter',    2000, ...      % nb max  d'itÈration
-                   'TolFun',     1e-16,  ...    % valeur critique fonction
-                   'TolX',       1e-16);         % tolÈrence itÈrÈs  
+skip=0;
+if isfile('sol.tmp')
+    disp('Previous solution found, updating it')
+    previous_sol=load('sol.tmp');
+    [new_sol,fval]=fminsearch(objectif,previous_sol,options);
+    if objectif(new_sol)<tol
+        skip=1;
+        disp('Skipping genetic algorithm, previous solutions was accurate enough after update')
+        disp(['Obj_fun=',num2str(fval),' c=', num2str(new_sol(1,1)), ' tb=', num2str(new_sol(1,2)),' r=' , num2str(new_sol(1,3))]);
+        vec_opt=new_sol;
+    else
+        disp('Previous solutions not accurate enough, starting from scratch')
+    end
+else
+    disp('Previous solution not found, starting from scratch')
+end
 
-%crÈation de la population intiale
-for i=1:1:nb_pop;
-    for j=1:1:nb_var;
-    pop(i,j)=borne*rand-2*borne*rand;
-    end;
-end;
-obj=1e9;
-nb=1;
+if skip==0;
+    %cr√©ation de la population intiale par algo de Mont√© Carlo
+    disp('Starting with a Mont√©-Carlo algorithm')
+    for i=1:1:20000
+        for j=1:1:nb_var
+            pop_ini(i,j)=borne*rand-2*borne*rand;
+        end
+        vecteur_initial(i,1)=objectif(pop_ini(i,:));
+    end
+    classement_ini=sortrows([pop_ini,vecteur_initial],nb_var+1);
+    %On r√©cup√®re les donn√©es class√©es
+    pop=classement_ini(1:nb_pop,1:nb_var);
+
+    disp('Starting genetic algorithm coupled to Nelder-Mead method')
+    %toc
+    obj=1e9;
+    nb=1;
+    % warning('off','all')
     while(tol<obj)
-nb=nb+1;
-    % on evalue la population et on repËre le meilleur et son indice
-    % on stocke le meilleur dans un tableau pour tracer l'Èvolution
-    %On crÈÈe une structure pour le classement des donnÈes
-    for i=1:1:nb_pop;
-        %on balance un petit simplex au passage
-        [pop(i,:)]=fminsearch(objectif,pop(i,:),options);
-        vec_objectif(i) = objectif(pop(i,:));
-    end;
-    classement=sortrows([pop,vec_objectif],nb_var+1);
-    %On rÈcupËre les donnÈes classÈes
-    pop=classement(:,[1:nb_var]);
+        nb=nb+1;
+        % on evalue la population et on rep√®re le meilleur et son indice
+        % on stocke le meilleur dans un tableau pour tracer l'√©volution
+        % on cr√©√©e une structure pour le classement des donn√©es
+        for i=1:nb_pop
+            %on balance un petit simplex au passage
+            [pop(i,:)]=fminsearch(objectif,pop(i,:),options);
+            vec_objectif(i) = objectif(pop(i,:));
+        end
 
-    % on duplique le meilleur 2 fois dans la nouvelle population
-    %remplissage ligne 1
-    popnew(1,:)=pop(1,:);
-    popnew(2,:)=pop(1,:);
-    
-    %on stocke l'historique
-    val(nb-1)=(objectif(pop(1,:)));
-   
-    % croisement 
-    for i=3:2:nb_pop
-        
-        % on selectionne 2 parents pour faire 2 enfants
-        % choix parmis les 20 meilleurs
-        i1=ceil(nb_crois*rand);
-        i2=ceil(nb_crois*rand);
-        %croisement et mutation
-        popnew=croisement_GA(i,i1,i2,pop,popnew,p_croisement,p_mutation,borne);
+        classement=sortrows([pop,vec_objectif],nb_var+1);
+        %On r√©cup√®re les donn√©es class√©es
+        pop=classement(:,1:nb_var);
 
+        % on duplique le meilleur 1 fois dans la nouvelle population
+        popnew(1,:)=pop(1,:);
+        %popnew(2,:)=pop(1,:);
+
+        %on stocke l'historique
+        val(nb-1)=(objectif(pop(1,:)));
+
+        % croisement
+        for i=3:2:nb_pop
+            % on selectionne 2 parents pour faire 2 enfants
+            % choix parmis les 20 meilleurs
+            i1=ceil(nb_crois*rand);
+            i2=ceil(nb_crois*rand);
+            %croisement et mutation
+            popnew=croisement_GA(i,i1,i2,pop,popnew,p_croisement,p_mutation,borne);
+        end
+        pop=popnew;
+        disp(['Epoch#',num2str(nb-1),' Obj_fun=',num2str(val(nb-1)),' c=', num2str(popnew(1,1)), ' tb=', num2str(popnew(1,2)),' r=' , num2str(popnew(1,3))]);
+        obj=val(nb-1);
+        if nb==max_steps
+            obj=0;
+            disp('Echec de la m√©thode, reload');
+        end
     end
-    
-    pop=popnew;
-
-    if rem(nb,1)==0;
-    disp(['Iteration #',num2str(nb-1),' Obj function=',num2str(val(nb-1))]);
-    disp(popnew(1,:));
-    end;
-    obj=val(nb-1);
-    if nb==10;
-        obj=0;
-        disp('Algorithm failed to find global minimum, reload');
-    end;
-    end
-    
-    % on affiche les rÈsultats  
-    
-    vec_opt=[popnew(1,:)];
     fval=obj;
+    vec_opt=[popnew(1,:)];
+end
+
+save('sol.tmp','vec_opt','-ascii')
 toc
